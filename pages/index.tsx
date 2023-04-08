5a82c4a3c7cfbcd748c96cf7aa2ce0a424a1ce49
Jesus Manuel Hernandez <jesus.herzo@outlook.com>
2023-04-07 20:18:12 -0600

    feat: removed navbar from build

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,6 @@ const Home: NextPage = () => {
         <meta name="description" content="Welcome to jmherzo.com" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navbar />
       <Landing />
     </>
   );
