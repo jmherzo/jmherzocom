3502d78448de1a86d21422715b0e902df95abae0
Jesus Hernandez Zozaya <jesus.herzo@outlook.com>
2025-03-06 21:58:20 -0600

    feat: jmherzocom 2.0

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jesus Manuel Hernandez Zozaya | Senior Software Engineer",
+  description:
+    "Personal portfolio of Jesus Manuel Hernandez Zozaya, a senior software engineer specialized in JavaScript, TypeScript, React and NodeJS",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
